8d1c5a2701bf5f85f411d3f4eafe634765a757bb
David Swatzell <thatsstrikethree@gmail.com>
2023-10-28 14:00:11 -0400

    completed first 3 examples.

--- cleanUp.js
+++ cleanUp.js
@@ -1,44 +1,47 @@
 // Each example below has at least one opportunity for improvement.
 
 // YOUR TASK:
-  // Modify the code to make that improvement(s)
-  // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
-    // using `node cleanUp.js` or copy and paste into a replit
+// Modify the code to make that improvement(s)
+// write a JS comment to explain what you changed, and why
+// make sure the code you submit WORKS - you can run this entire file 
+// using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+function askForName() {
+    return ("Hello, what is your name?")
+}
 
-askForName()
-
+console.log(askForName())
+    // askForName() I took out that xtra askForName because it was unnecesary and turned it into a proper function with a return value.
 
 // EX 2:
-function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+function addThreeNums(first, second, third) {
+    var sum = first + second + third
+    return sum;
+}
+
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
+
+// I added console.log so that it would return the sum of the numbers
 
 
 // EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
+function makeFreshPesto() {
+    return ("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper. Pulse basil and pine nuts. Add garlic and cheeses. Slowly pour in oil.Season");
+}
 
-makeFreshPesto();
+console.log(makeFreshPesto());
+// took out all the individual console.logs and just made it to return the recipe when called
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
+function average(num1, num2) {
+    var sum = num1 + num2;
     var avg = sum / 2;
 
-return avg
-  }
+    return avg
+}
+console.log(avg)
